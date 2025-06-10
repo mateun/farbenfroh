0f528a7fd476262e3ee6e354f335f357d7639746
gru <martingruscher@gmail.com>
2025-06-10 15:25:12 +0200

    postprocess and general api enhancments

--- engine/renderer/vulkan/src/vulkan_impl.cpp
+++ engine/renderer/vulkan/src/vulkan_impl.cpp
@@ -2071,7 +2071,7 @@ void VulkanRenderer::beginCustomRenderPass(VkCommandBuffer commandBuffer, VkRend
     renderPassInfo.renderArea.offset = {0, 0};
     renderPassInfo.renderArea.extent = {width, height};
     std::array<VkClearValue,2> clearValues{};
-    clearValues[0].color = {{0.1f, 0.0f, 0.4f, 1.0f}};
+    clearValues[0].color = {{0.0f, 0.0f, 0.0f, 1.0f}};
     clearValues[1].depthStencil = {1.0f, 0};
 
     renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
