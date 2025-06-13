30d564b6c8c6fe0a07b6b69c5670fccfc0e95ec9
gru <martingruscher@gmail.com>
2025-06-13 12:21:23 +0200

    api changes

--- engine/renderer/vulkan/src/vulkan_impl.cpp
+++ engine/renderer/vulkan/src/vulkan_impl.cpp
@@ -921,7 +921,8 @@ std::tuple<VkPipeline, VkPipelineLayout> VulkanRenderer::createGraphicsPipeline(
     rasterizer.depthClampEnable = VK_FALSE;
     rasterizer.depthBiasEnable = VK_FALSE;
     rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
-    rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+    //rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+    //rasterizer.cullMode = VK_CULL_MODE_NONE;
     //rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
     rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
     rasterizer.lineWidth = 1.0f;
